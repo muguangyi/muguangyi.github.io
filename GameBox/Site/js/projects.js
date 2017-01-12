68279b26f20565c0b806c27378155868660c7354
muguangyi <muguangyi@gmail.com>
2017-01-12 18:04:00 +0800

    Update.

--- GameBox/Site/js/projects.js
+++ GameBox/Site/js/projects.js
@@ -1,7 +1,7 @@
 $(function(){
 
 var sitenavbar = "<div id=\"site-navbar\" style=\"position: absolute; top: -4px; left: -3px; border: 0; z-index: 2000;padding:0;margin:0;\">" +
-			"<a href=\"/\" title=\"返回首页\" style=\"background:none;\">" +
+			"<a href=\"/GameBox/Site/index.html\" title=\"返回首页\" style=\"background:none;\">" +
 				"<img src=\"./p/return-back.png\" style=\"padding:0;margin:0;border:0; -webkit-box-shadow: none;-moz-box-shadow: none;box-shadow: none;\">"+
 			"</a>"+
 		"</div>";
